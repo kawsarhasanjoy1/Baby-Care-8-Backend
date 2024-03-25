b3ffca26eee586fe48f4d77b9b5eccc1de670ff0
kawsar hasan joy <kawsarhasanjoy342@gmail.com>
2024-03-25 15:10:50 +0600

    my second commit

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 5000;
-require("dotenv").config();
+
+
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
