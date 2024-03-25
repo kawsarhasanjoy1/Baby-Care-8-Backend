9b1c83a56bb6d89871cdf7d317fe848983c7386c
kawsar hasan joy <kawsarhasanjoy342@gmail.com>
2024-03-25 13:25:15 +0600

    my first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+require("dotenv").config();
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = process.env.MONGODB_URL;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const productsCollection = client.db("BabyCare-8").collection("products");
+
+    // Send a ping to confirm a successful connection
+
+    app.get("/products", async (req, res) => {
+      const result = await productsCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/flash-sale", async (req, res) => {
+      const falseSale = await productsCollection
+        .find()
+        .sort({ createdAt: 1 })
+        .toArray();
+      const filter = falseSale.filter((flash) => flash.isFlash == true);
+      res.send(filter);
+    });
+    app.get("/brands/:category", async (req, res) => {
+      const category = req.params.category;
+      const query = { category: category };
+      const result = await productsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
