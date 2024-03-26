ee4c098120312a7779f88d1454a1e049bc477e1a
kawsar hasan joy <kawsarhasanjoy342@gmail.com>
2024-03-26 12:01:34 +0600

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -35,7 +35,7 @@ async function run() {
       const result = await productsCollection.find().toArray();
       res.send(result);
     });
-    
+
     app.get("/product/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
