5d2713368fc136bba441a899dd95eaa40a67d889
kawsar hasan joy <kawsarhasanjoy342@gmail.com>
2024-04-23 08:58:38 +0600

    my first commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -27,9 +29,62 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const userCollection = client.db("BabyCare-8").collection("user");
     const productsCollection = client.db("BabyCare-8").collection("products");
+    const orderCollection = client.db("BabyCare-8").collection("orders");
 
     // Send a ping to confirm a successful connection
+
+    app.post("/api/v1/register", async (req, res) => {
+      const { Name, Email, Password } = req.body;
+      // Check if email already exists
+      const existingUser = await userCollection.findOne({ Email });
+      if (existingUser) {
+        return res.status(400).json({
+          success: false,
+          message: "User already exists",
+        });
+      }
+
+      // Hash the password
+      const hashedPassword = await bcrypt.hash(Password, 10);
+
+      // Insert user into the database
+      await userCollection.insertOne({ Name, Email, Password: hashedPassword });
+
+      res.status(201).json({
+        success: true,
+        message: "User registered successfully",
+      });
+    });
+
+    // User Login
+    app.post("/api/v1/login", async (req, res) => {
+      const { Email, Password } = req.body;
+      // Find user by email
+      const user = await userCollection.findOne({ Email });
+
+      if (!user) {
+        return res.status(401).json({ message: "Invalid email or password" });
+      }
+
+      // Compare hashed password
+      const isPasswordValid = await bcrypt.compare(Password, user.Password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: "Invalid email or password" });
+      }
+
+      // Generate JWT token
+      const token = jwt.sign({ email: user.Email }, process.env.JWT_SECRET, {
+        expiresIn: process.env.EXPIRES_IN,
+      });
+
+      res.json({
+        success: true,
+        message: "Login successful",
+        token,
+      });
+    });
 
     app.get("/products", async (req, res) => {
       const queryUrl = req.query;
@@ -46,8 +101,6 @@ async function run() {
         };
       }
 
-      console.log(modifyQuery);
-
       const result = await productsCollection.find(modifyQuery).toArray();
       res.send(result);
     });
@@ -59,14 +112,6 @@ async function run() {
       res.send(result);
     });
 
-    // app.get(`/products`, async (req, res) => {
-    //   const category = req.params.category;
-    //   console.log(req.params)
-    //   const query = { category: category };
-    //   const result = await productsCollection.find(query).toArray();
-    //   res.send(result);
-    // });
-
     app.get("/flash-sale", async (req, res) => {
       const falseSale = await productsCollection
         .find()
@@ -75,6 +120,12 @@ async function run() {
 
       const filter = falseSale.filter((flash) => flash.isFlash == true);
       res.send(filter);
+    });
+
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
     });
 
     await client.db("admin").command({ ping: 1 });
