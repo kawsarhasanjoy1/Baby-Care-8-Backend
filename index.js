80101e9d56140ea2ed5c38d4db33e06f33602d66
kawsar hasan joy <kawsarhasanjoy342@gmail.com>
2024-05-03 11:31:07 +0600

    project finished

--- index.js
+++ index.js
@@ -29,16 +29,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const userCollection = client.db("BabyCare-8").collection("user");
-    const productsCollection = client.db("BabyCare-8").collection("products");
-    const orderCollection = client.db("BabyCare-8").collection("orders");
+    const userCollection = client.db("BabyCare-9").collection("user");
+    const productsCollection = client.db("BabyCare-9").collection("products");
+    const orderCollection = client.db("BabyCare-9").collection("order");
 
     // Send a ping to confirm a successful connection
 
     app.post("/api/v1/register", async (req, res) => {
-      const { Name, Email, Password } = req.body;
+      const { name, email, password } = req.body;
       // Check if email already exists
-      const existingUser = await userCollection.findOne({ Email });
+      const existingUser = await userCollection.findOne({ email });
       if (existingUser) {
         return res.status(400).json({
           success: false,
@@ -47,10 +47,10 @@ async function run() {
       }
 
       // Hash the password
-      const hashedPassword = await bcrypt.hash(Password, 10);
+      const hashedPassword = await bcrypt.hash(password, 10);
 
       // Insert user into the database
-      await userCollection.insertOne({ Name, Email, Password: hashedPassword });
+      await userCollection.insertOne({ name, email, password: hashedPassword });
 
       res.status(201).json({
         success: true,
@@ -60,22 +60,22 @@ async function run() {
 
     // User Login
     app.post("/api/v1/login", async (req, res) => {
-      const { Email, Password } = req.body;
+      const { email, password } = req.body;
       // Find user by email
-      const user = await userCollection.findOne({ Email });
+      const user = await userCollection.findOne({ email });
 
       if (!user) {
         return res.status(401).json({ message: "Invalid email or password" });
       }
 
       // Compare hashed password
-      const isPasswordValid = await bcrypt.compare(Password, user.Password);
+      const isPasswordValid = await bcrypt.compare(password, user.password);
       if (!isPasswordValid) {
         return res.status(401).json({ message: "Invalid email or password" });
       }
 
       // Generate JWT token
-      const token = jwt.sign({ email: user.Email }, process.env.JWT_SECRET, {
+      const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, {
         expiresIn: process.env.EXPIRES_IN,
       });
 
@@ -84,6 +84,19 @@ async function run() {
         message: "Login successful",
         token,
       });
+    });
+
+    app.get("/api/v1/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      const result = await productsCollection.insertOne(product);
+      res.send(result);
     });
 
     app.get("/products", async (req, res) => {
@@ -105,11 +118,43 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/product/:productId", async (req, res) => {
+      try {
+        const id = req.params.productId;
+        const query = { _id: new ObjectId(id) };
+        const result = await productsCollection.deleteOne(query);
+        res.send(result);
+      } catch (err) {
+        res.send(err);
+      }
+    });
+    app.patch("/product/:productId", async (req, res) => {
+      try {
+        const id = req.params.productId;
+        const product = req.body;
+        console.log(id, product);
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            product,
+          },
+        };
+        const result = await languageCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      } catch (err) {
+        res.send(err);
+      }
+    });
+
     app.get("/product/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await productsCollection.find(query).toArray();
-      res.send(result);
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await productsCollection.find(query).toArray();
+        res.send(result);
+      } catch (err) {
+        res.send(err);
+      }
     });
 
     app.get("/flash-sale", async (req, res) => {
@@ -122,12 +167,26 @@ async function run() {
       res.send(filter);
     });
 
-    app.post("/orders", async (req, res) => {
+    app.post("/order", async (req, res) => {
       const order = req.body;
-      const result = await orderCollection.insertOne(order);
+      console.log(order);
+      const result = orderCollection.insertOne(order);
+      res.send({
+        message: "order successful",
+        data: result,
+      });
+    });
+    app.get("/order", async (req, res) => {
+      const result = orderCollection.find().toArray;
       res.send(result);
     });
-
+    app.get("/order/:email", async (req, res) => {
+      const email = req.params.email;
+      console.log(email);
+      const query = { email: email };
+      const result = await orderCollection.find(query).toArray();
+      res.send(result);
+    });
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
